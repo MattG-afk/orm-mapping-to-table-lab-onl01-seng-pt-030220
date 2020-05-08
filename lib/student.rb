6db73e558fb44064247a343a33657d25dec1ae36
Matt Gordon <bigmattsize21@gmail.com>
2020-05-08 01:00:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,8 @@ class Student
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY)
+        id INTEGER PRIMARY KEY,
+        )
   
   
   
